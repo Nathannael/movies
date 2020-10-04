de70dc9c0b4660e1875fe05d1a84f7554e66a73c
Nathannael Morais <Nathannael2009@gmail.com>
2020-10-04 11:03:11 -0300

    Initial implementation for OMDb API integration
    The folder /integrations will contain the client for external APIs.
    For this project's purposes, only the OMDb client will actually matter
    but it's important to have everything already in the right folders.
    
    Web requests were mocked using webmock.

---
+++ app/integrations/omdb/client.rb
@@ -0,0 +1,38 @@
+module OMDb
+  class Client
+    include HTTParty
+
+    base_uri 'http://www.omdbapi.com'
+
+    def self.find_by_id(id:)
+      url_params = "i=#{id}"
+
+      call(url_params)
+    end
+
+    def self.find_by_name(name:)
+      url_params = "t=#{name}"
+
+      call(url_params)
+    end
+
+    def self.search(term:)
+      url_params = "s=#{term}"
+
+      call(url_params)
+    end
+
+    private
+    def self.call(url_params)
+      result = get("/?#{url_params}&apikey=#{api_key}")
+    rescue => e
+      OpenStruct.new({success?: false, error: e.message})
+    else
+      OpenStruct.new({success?: true, payload: result})
+    end
+
+    def self.api_key
+      Rails.application.credentials.omdb[:access_key]
+    end
+  end
+end
---
+++ spec/integrations/omdb/client_spec.rb
@@ -0,0 +1,73 @@
+require 'rails_helper'
+
+RSpec.describe OMDb::Client do
+  let(:api_key) { Faker::Alphanumeric.alphanumeric(number: 8) }
+  let(:successful_response) { 'content' }
+
+  before do
+    allow(described_class).to receive(:api_key).and_return(api_key)
+  end
+
+  shared_examples 'endpoint' do
+    context 'when API is available' do
+      before do
+        stub_request(:get, 'www.omdbapi.com')
+          .with(query: query.merge({apikey: api_key}) )
+          .to_return(body: successful_response)
+      end
+
+      it 'returns success = true' do
+        expect(request).to be_success
+      end
+
+      it 'returns payload from OMDb API' do
+        expect(request.payload.body).to eq successful_response
+      end
+    end
+
+    context 'when request returns an error' do
+      let(:error) { 'API not Available' }
+
+      before do
+        stub_request(:get, 'www.omdbapi.com')
+          .with(query: query.merge({apikey: api_key}) )
+          .to_raise(error)
+      end
+
+      it 'returns success = false' do
+        expect(request).not_to be_success
+      end
+
+      it 'returns error message' do
+        expect(request.error).to eq error
+      end
+    end
+  end
+
+  describe '.find_by_id' do
+    let(:id) { Faker::Alphanumeric.alphanumeric(number: 8) }
+
+    it_behaves_like 'endpoint' do
+      let(:query) { { i: id } }
+      let(:request) { described_class.find_by_id(id: id) }
+    end
+  end
+
+  describe '.find_by_name' do
+    let(:name) { Faker::Movie }
+
+    it_behaves_like 'endpoint' do
+      let(:query) { { t: name } }
+      let(:request) { described_class.find_by_name(name: name) }
+    end
+  end
+
+  describe '.search' do
+    let(:term) { Faker::Movie }
+
+    it_behaves_like 'endpoint' do
+      let(:query) { { s: term } }
+      let(:request) { described_class.search(term: term) }
+    end
+  end
+end
